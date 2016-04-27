da7f8512d05a9dba12ab5e47a014803fd95c4d1d
debjeetdas <debjeetster@gmail.com>
2016-04-27 23:18:48 +0530

    orientation update_Homepage_mobile
    orientation update_Homepage_mobile

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -2059,3 +2059,14 @@ $("#company-anime-banner path").animate({"opacity":"1"},5000);
      });
 
 });
+
+/* Homepage hack */
+
+// $(document).ready(function () {
+//   function reorient(e) {
+//     var portrait = (window.orientation % 180 == 0);
+//     $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+//   }
+//   window.onorientationchange = reorient;
+//   window.setTimeout(reorient, 0);
+// });
