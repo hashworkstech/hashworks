03008546abaf81bfb8b7a78e424725fcbb0dc871
rakshithahc1 <rakshitha.chandra@gmail.com>
2016-05-19 10:59:40 +0530

    responsive status done

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -2131,36 +2131,46 @@ function rotate(el, degs) {
 // //     });
 // });
 // var count=0;
-// $('.eng-zoom-in').click(function () {
+$('.eng-zoom-in').click(function () {
    // if(width < 100+"%"){
   // count++;
   // if(count<=3){
-  // $(".zoomin img").animate({
-  //           width: "+=10%"
-  //       });
+  $(".zoomin1 img").animate({
+            width: "+=10%"
+        });
   // }
-   // });
+   });
 // var count1=0;
-// $('.eng-zoom-out').click(function () {
-   // if(width < 100+"%"){
-  // count1++;
-  // if(count1<=3){
-  // $(".zoomin img").animate({
-  //           width: "-=10%"
-  //       });
-  // }
-   // });
-// setTimeout(function(){
-//    document.getElementById("myDiv").style.display="hide";
-// }, 5000);  
-var width=$(".tab-img").width();
-$('.eng-zoom-in').click(function () {
-  if(width < 130+"%"){
+$('.eng-zoom-in1').click(function () {
   $(".zoomin img").animate({
             width: "+=10%"
         });
-}
+  });
+$('.eng-zoom-out').click(function () {
+   // if(width < 100+"%"){
+  // count1++;
+  // if(count1<=3){
+  $(".zoomin1 img").animate({
+            width: "-=10%"
+        });
+  // }
    });
+$('.eng-zoom-out1').click(function () {
+  $(".zoomin img").animate({
+            width: "-=10%"
+        });
+   });
+// setTimeout(function(){
+//    document.getElementById("myDiv").style.display="hide";
+// }, 5000);  
+// var width=$(".tab-img").width();
+// $('.eng-zoom-in').click(function () {
+//   if(width < 130+"%"){
+//   $(".zoomin img").animate({
+//             width: "+=10%"
+//         });
+// }
+//    });
 
 (function(){
     var myDiv = document.getElementById("logo-img");
