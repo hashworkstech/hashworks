5abbe07005089647e4f122008d7a30a942929b8a
rakshithahc1 <rakshitha.chandra@gmail.com>
2016-06-10 18:01:19 +0530

    changes in js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2206,8 +2206,8 @@ body.mozrequestFullscreen();
 } else if (body.msrequestFullscreen) {
 body.msrequestFullscreen();
 }
-window.addEventListener("load", function() { window. scrollTo(0, 0); });
-document.addEventListener("touchmove", function(e) { e.preventDefault() });
+// window.addEventListener("load", function() { window. scrollTo(0, 0); });
+// document.addEventListener("touchmove", function(e) { e.preventDefault() });
 // var expanded = false;
 //     function showCheckboxes() {
 //         var checkboxes = document.getElementById("cat");
