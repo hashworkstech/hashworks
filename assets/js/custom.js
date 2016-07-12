ff771efd37e9001b3bb2f4d1c51929437fa623b9
rakshithahc1 <rakshitha.chandra@gmail.com>
2016-07-13 00:29:30 +0530

    changes in pan zoom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2121,7 +2121,20 @@ function rotate(el, degs) {
   $(el).css(styles);
 }
 
-
+(function() {
+          var $section = $('#focal');
+          var $panzoom = $section.find('.panzoom').panzoom();
+          $panzoom.parent().on('mousewheel.focal', function( e ) {
+            e.preventDefault();
+            var delta = e.delta || e.originalEvent.wheelDelta;
+            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+            $panzoom.panzoom('zoom', zoomOut, {
+              increment: 0.1,
+              animate: false,
+              focal: e
+            });
+          });
+        })();
 
 // $('.eng-zoom-in').click(function () {
 //      var w1=$(".zoomin1 img").width()/$(".zoomin1 img").parent().width() * 100+"%";
